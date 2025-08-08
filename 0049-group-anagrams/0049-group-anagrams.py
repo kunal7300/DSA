2e9eb99a9605cb3e48a13be1420d89e1d99a38d7
kunal sharma <renintubule@gmail.com>
2025-08-09 01:12:13 +0530

    Update 0049-group-anagrams.py

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -5,4 +5,4 @@ class Solution:
         letters_to_words = defaultdict(list)
         for word in strs:
             letters_to_words[tuple(sorted(word))].append(word)
-        return list(letters_to_words.values())yghjyr
+        return list(letters_to_words.values())
